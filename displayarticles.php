48b042c5ab4401bea3095cdf33ec9730c1c2a94c
adharshinic <161732729+adharshinic@users.noreply.github.com>
2024-05-30 19:53:54 +0530

    Add files via upload

---
+++ displayarticles.php
@@ -0,0 +1,54 @@
+<?php
+include("header.php");
+?>
+  <main id="main">
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="text-center" data-aos="zoom-in">
+			<br>
+			<br>
+          <h3>EXHIBITION DETAILS</h3>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+ 
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="row">
+ 
+          <div class="col-lg-12">
+            <div class="row">
+
+<html>
+<div class="col-lg-4">
+	<div class="member" data-aos="zoom-in" data-aos-delay="100">
+	  <div class=""><a href='displayarticlesdetailed.php'> <a> <img src='images/th.jpg'  width='100%' height='150' style='padding-right: 10px;'>CHENNAI-KALAUTSAV</a></div>
+	  <div class="member-info">
+		<h4><a href='displayarticlesdetailed.php'></a></h4>
+		<span>DATE - JUNE 17,2024 </span>
+		  <a href="displayarticlesdetailed.php" class="btn btn-danger">Read More..</a>
+	  </div>
+	</div>
+</div>
+<?php
+
+?>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+  </main><!-- End #main -->
+  
+<?php
+include("footer.php");
+?>
---
+++ displayarticlesdetailed.php
@@ -0,0 +1,103 @@
+<?php
+include("header.php");
+
+?>
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="text-center" data-aos="zoom-in">
+		<br>
+		<br>
+		<br>
+          <h3>CHENNAI KALAUTSAV-RURAL ARTISANS EXHIBITION</h3>
+		  <p>DATE - JUNE 17TH 2024</p>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+  <main id="main">
+
+
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row content">
+          <div class="col-lg-12 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
+            <p class="font-italic"><a>
+			<img src="images\blog1.jpg"><a>
+			"Crafts Utsavam @ Kathipara!<br><br>
+
+
+
+
+      The best curated Indian Arts, Crafts, Fashion, and jewelry to meet all your shopping needs!
+
+Crafts Utsavam is an amalgamation of handcrafted products and handwoven textiles by established craft groups and NGOs. More than 100 stalls have been selected to exhibit the best of arts and crafts!
+
+So block your date for an extravaganza shopping experience!
+Visit and check the best artistic creations from our indigenous artists and craftspeople from all over India!!
+
+Date – 23 Feb to 10 Mar 2024
+Timings – 10.30 am onwards
+Venue – Uptown Kathipara, Under Metro Station, Kathipara Junction, Guindy Kathipara Bridge, near Alandur, St.Thomas Mount, Tamil Nadu 600016"</p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container">
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-6 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap"><a>
+ <a><img src='images\blog8.jpg'  class='img-fluid' align='left' style='padding-right: 10px;width: 100%;'>
+
+
+            </div>
+          </div>
+
+
+          <div class="col-lg-6 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+
+<a><img src='images\blog7.jpg'  class='img-fluid' align='left' style='padding-right: 10px;width: 100%;'>
+
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+
+<a><img src='images\blog6.jpg'  class='img-fluid' align='left' style='padding-right: 10px;width: 100%;'>
+            </div>
+          </div>
+
+
+          <div class="col-lg-6 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+
+  </main><!-- End #main -->
+  
+<?php
+include("footer.php");
+?>
